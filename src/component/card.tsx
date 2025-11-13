8a1492bad529c7f0edba9ed6bc49a547744db043
Adriano047 <106997880+Adriano047@users.noreply.github.com>
2025-11-13 21:58:24 +0000

    corrgindo problema no merge de participantes

--- src/component/card.tsx
+++ src/component/card.tsx
@@ -7,26 +7,15 @@ type CardProps = {
   link?: string
 }
 
-<<<<<<< HEAD
 export default function Card({ imagemLocal, nome, localizacao, link }: CardProps) {
-=======
-export default function Card({ imagemLocal, nome, localizacao, link}: CardProps) {
->>>>>>> feat/participantes
   return (
     <div className="card">
       <div className="cabecalho-card">
         <img className="imagemLocal" src={imagemLocal} alt="" />
       </div>
-<<<<<<< HEAD
       <h2>{nome}</h2>
       <p>{localizacao}</p>
       <Botao texto="Ver no Mapa" icone={IconeDeMapa} link={link} />
-=======
-      
-      <h2>{nome}</h2>
-      <p>{localizacao}</p>
-      <Botao texto="Ver no Mapa" icone={IconeDeMapa} link={link}/>
->>>>>>> feat/participantes
     </div>
   )
 }
