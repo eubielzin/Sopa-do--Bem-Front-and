f3619c3e8a6bebb396a354036d0e6d54ab52e60e
Adriano047 <106997880+Adriano047@users.noreply.github.com>
2025-11-17 23:22:03 +0000

    ajustando o tamanho dos cards

--- src/sections/DeliveryPoints/pontosEntrega.tsx
+++ src/sections/DeliveryPoints/pontosEntrega.tsx
@@ -1,7 +1,8 @@
+import styles from "./pontosEntrega.module.css"
 //Estrutura dos cards
 import Card from "../../component/card"
 //Estilização deste arquivo
-import "./pontosEntrega.css"
+
 //Importações da imagens
 import PracaDeodoro from "../../assets/localdeodoro.png"
 import MercadoCentral from "../../assets/localmercado-central.png"
@@ -10,14 +11,14 @@ import HospitalGeral from "../../assets/localhospital-geral.png"
 import SantaCasa from "../../assets/localsanta-casa.png"
 export function Entrega() {
     return (
-        <div className="secaoEntrega">
-            <h1>Nosso Pontos De Entrega</h1>
-            <div className="Cards">
-            <Card imagemLocal={PracaDeodoro} nome="Praça deodoro" localizacao="Avenida Gomes de Castro, Centro" link="https://surl.li/tjoswc" />
-            <Card imagemLocal={MercadoCentral} nome="Mercado Central" localizacao="Av. Guaxenduba, Centro" link="https://surl.lt/xmlrcb"/>
-            <Card imagemLocal={SocorraoI} nome="Socorrão I" localizacao="Rua do Passeio, S/N, Centro" link="https://surl.li/xwwucd"/>
-            <Card imagemLocal={HospitalGeral} nome="Hospital Geral" localizacao="R. São Pantaleão, 02 , Centro" link="https://surl.li/qmhkro"/>
-            <Card imagemLocal={SantaCasa} nome="Santa Casa" localizacao="R. do Norte, 233, Centro" link="https://surl.li/brltvu"/>
+        <div className={styles.secaoEntrega}>
+            <h1>Nossos Pontos De Entrega</h1>
+            <div className={styles.Cards}>
+            <Card estiloCard={styles} imagemLocal={PracaDeodoro} nome="Praça deodoro" localizacao="Avenida Gomes de Castro, Centro" link="https://surl.li/tjoswc" />
+            <Card estiloCard={styles} imagemLocal={MercadoCentral} nome="Mercado Central" localizacao="Av. Guaxenduba, Centro" link="https://surl.lt/xmlrcb"/>
+            <Card estiloCard={styles} imagemLocal={SocorraoI} nome="Socorrão I" localizacao="Rua do Passeio, S/N, Centro" link="https://surl.li/xwwucd"/>
+            <Card estiloCard={styles} imagemLocal={HospitalGeral} nome="Hospital Geral" localizacao="R. São Pantaleão, 02 , Centro" link="https://surl.li/qmhkro"/>
+            <Card estiloCard={styles} imagemLocal={SantaCasa} nome="Santa Casa" localizacao="R. do Norte, 233, Centro" link="https://surl.li/brltvu"/>
             </div>
         </div>
     )
