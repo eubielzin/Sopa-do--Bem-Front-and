dad4fa4d267004988c3db401835c3f0068493be4
juliocesarff <juliochavesaraujo.6@gmail.com>
2025-11-19 11:40:52 -0300

    ajuste do header feito

--- src/sections/Header/header.tsx
+++ src/sections/Header/header.tsx
@@ -8,12 +8,12 @@ export function Header() {
                     <img className={styles.logo} src="src/assets/logo.png" alt="" />
 
                     <ul className={styles['nav-links']} >
-                        <li> <a href="Membros">Membros</a></li>
-                        <li> <a href="Entregas">Entregas</a></li>
-                        <li> <a href="Ajude-nós">Ajude-nós</a></li>
-                        <li> <a href="Pontos de Entregas">Pontos de Entregas</a></li>
-                        <li> <a href="Parcerias">Parcerias</a></li>
-                        <li> <a href="Fale conosco">Fale conosco</a></li>                        
+                        <li> <a href="#Fundadores">Fundadores</a></li>
+                        <li> <a href="#Galeria">Galeria</a></li>
+                        <li> <a href="#Donate">Doações</a></li>
+                        <li> <a href="#Calendario">Calendario</a></li>                       
+                        <li> <a href="#Entrega">Pontos de Entregas</a></li>
+                        <li> <a href="#Parcerias">Parcerias</a></li>                        
                     </ul>
                     <button className={styles.botao}> <a href="Aniversariantes">Aniversariantes</a></button>
                 </div>
