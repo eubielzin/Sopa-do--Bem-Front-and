9367f9590c5a2369e9094cd8738b1b50d45ac86f
Adriano047 <adrianocardoso047@gmail.com>
2025-11-20 12:36:37 -0300

    arrumando a galeria, adicionando responsividade global, e adicionando gap para separar as seções

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,21 @@ import { Header } from './sections/Header/header.tsx'
 import { Introducao } from './sections/Introduction/introducao.tsx'
 import { Fundadores } from './sections/Founders/fundadores.tsx'
 import { Galeria } from "./sections/Gallery/galeria.tsx"
-import {Donate} from './sections/Donate/donate.tsx'
-import {Calendario} from './sections/Calendar/calendario.tsx'
-import {Entrega} from './sections/DeliveryPoints/pontosEntrega.tsx'
+import { Donate } from './sections/Donate/donate.tsx'
+import { Calendario } from './sections/Calendar/calendario.tsx'
+import { Entrega } from './sections/DeliveryPoints/pontosEntrega.tsx'
 import { Prestacao } from "./sections/FinancialReport/prestacao.tsx"
-import {Footer} from './sections/Footer/footer.tsx'
+import { Footer } from './sections/Footer/footer.tsx'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className='containerPrincipal'>
         <Header />
         <Introducao />
         <Fundadores />
-        {/* <Galeria /> */}
+        <Galeria />
         <Donate />
         <Calendario />
         <Entrega />
